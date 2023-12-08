06eeddcffeca83dfe57124d6b4f897b5e1f27baa
Lee-Z-Han <cindy20050116@gmail.com>
2023-12-08 11:46:16 +0800

    week13

--- week13/week13-5b.py
+++ week13/week13-5b.py
@@ -1,4 +1,4 @@
-# SOIT106_ADVANCE_009
+# SOIT106_ADVANCE_009 Method 1
 a = input()
 N = len(a)
 for i in range(N-1, -1, -1):
--- week13/week13-5c.py
+++ week13/week13-5c.py
@@ -1,3 +1,4 @@
+# SOIT106_ADVANCE_009 Method 2
 a = int(input())
 while a>0:
 	print(a%10, end='')
